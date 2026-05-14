ba7c557139ad5affe06b9286f4faae7c5255bff3
nitish1238 <95521444+nitish1238@users.noreply.github.com>
2026-05-14 22:09:44 +0530

    71217

--- orders/models.py
+++ orders/models.py
@@ -10,4 +10,11 @@ class Order(models.Model):
     status=models.ForeignKey(OrderStatus,ondelete=models.SET_NULL,null=True)
     def __str__(self):
         return self.customer_name
-
+class Coupon(models.Model):
+    code=models.CharField(max_length=50,unique=True)
+    discount_percentage=models.DecimalField(max_digits=5,decimal_places=2)
+    is_active=BooleanField(default=True)
+    vaild_form=models.DateFiled()
+    vaild_until=models.DateFiled()
+    def __str__(self):
+        return self.code
