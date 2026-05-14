b776d3f4728bc44e27565a9be3becb7750127cb7
nitish1238 <95521444+nitish1238@users.noreply.github.com>
2026-05-14 21:54:52 +0530

    71215

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class OrderStatus(models.Model):
+    name=models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+class Order(models.Model):
+    customer_name=models.CharField(max_length=100)
+    total_amount=models.DecimalField(max_digits=10,decimal_places=2)
+    status=models.ForeignKey(OrderStatus,ondelete=models.SET_NULL,null=True)
+    def __str__(self):
+        return self.customer_name
+
