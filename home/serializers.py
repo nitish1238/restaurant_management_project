418584a3cd5e9bdaa805fdd63697e4276d6db7db
nitish1238 <95521444+nitish1238@users.noreply.github.com>
2026-05-16 13:26:16 +0530

    71222

--- home/serializers.py
+++ home/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from .models import MenuCategory 
-class MenuCategorySerializer(serializers.modelSerializer):
+class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=MenuCategory
         fields=['name']
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=MenuItem
+        fields="__all__"
+        
