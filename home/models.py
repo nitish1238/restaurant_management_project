3c4d55bbbb27e747d7fbeb6582fbc2654507625a
nitish1238 <95521444+nitish1238@users.noreply.github.com>
2026-05-16 13:24:15 +0530

    71222

--- home/models.py
+++ home/models.py
@@ -9,3 +9,9 @@ class Restaurant(models.Model):
     has_delivery=models.BooleanField(default=False)
     def __str__(self):
         return self.name
+class MenuItem(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+    is_featured=models.BooleanField(default=False)
+    def __str__(self):
+        return self.name
