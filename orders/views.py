9fde20caaf3fce9d8c723c763a23ddaf3df737b0
nitish1238 <95521444+nitish1238@users.noreply.github.com>
2026-05-16 12:43:16 +0530

    71221

--- orders/views.py
+++ orders/views.py
@@ -2,8 +2,12 @@ from datetime import date
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 from .models import Coupon
-class CouponVaildationView(APIView):
+from .models import Order
+from .serializers import OrderSerializer
+class CouponValidationView(APIView):
     def post(self,request):
         code=request.data.get("code")
         if not code:
@@ -13,14 +17,19 @@ class CouponVaildationView(APIView):
         try:
             coupon =Coupon.objects.get(
                 code=code,is_active=True,
-                valid_from_lte=today,
-                valid_until_gte=today)
-        expect Coupon.DoesNotExist:
+                valid_from__lte=today,
+                valid_until__gte=today)
+        except Coupon.DoesNotExist:
             return Response({"error":"Invalid or expired coupon."},
             status=status.HTTP_400_BAD_REQUEST
             )
         return Response({
             "valid":True,
-            "code":coupon.code
+            "code":coupon.code,
             "discount_percentage":coupon.discount_percentage
         },status=status.HTTP_200_OK)
+class OrderHistoryView(ListAPIView):
+    serializer_class=OrderSerializer
+    permission_classes=[IsAuthenticated]
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user).order_by("-id")
