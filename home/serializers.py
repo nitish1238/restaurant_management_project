06937602e5697035f10fa5ed9bdce94fee467e6f
nitish1238 <95521444+nitish1238@users.noreply.github.com>
2026-05-16 13:26:35 +0530

    71222

--- home/serializers.py
+++ home/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import MenuCategory 
+from .models import MenuCategory ,MenuItem
 class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=MenuCategory
