d847cdcec47d64b04dfd2ab2e6b584e65f01e7d0
nitish1238 <95521444+nitish1238@users.noreply.github.com>
2026-05-16 11:29:40 +0530

    71220

--- home/utils.py
+++ home/utils.py
@@ -1,2 +1,9 @@
 from datetime import datetime
-from .models import Daily
+from .models import DailyOperationsHours
+def get_today_operating_hours():
+    today=datetime.today().strftime("%A")
+    try:
+        hours=DailyOperationsHours.objects.get(day=today)
+        return hours.open_time,hours.close_time
+    expcet DailyOperationsHours.DoesNotExist:
+        return None, None
