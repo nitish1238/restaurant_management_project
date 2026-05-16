b9faa7ca697fb3568cb724e6f854b042956c1183
nitish1238 <95521444+nitish1238@users.noreply.github.com>
2026-05-16 11:20:33 +0530

    71219

--- home/models.py
+++ home/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 class MenuCategory(models.Model):
     name=models.CharField(max_length=100,unique=True)
+    
+    def __str__(self):
+        return self.name
+class Restaurant(models.Model):
+    name=models.CharField(max_length=100)
+    has_delivery=models.BooleanField(default=False)
     def __str__(self):
         return self.name
