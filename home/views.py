d2d9d986f9dde60ec98d30603dd4e71764a1da93
nitish1238 <95521444+nitish1238@users.noreply.github.com>
2026-05-16 13:31:03 +0530

    71222

--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from rest_framework.generics import ListAPIView
-from .models import MenuCategory
-from .serializers import MenuCategorySerializer
+from .models import MenuCategory,MenuItem
+from .serializers import MenuCategorySerializer, MenuItemSerializer
 class MenuCategoryListView(ListAPIView):
     queryset=MenuCategory.objects.all()
     serializer_class=MenuCategorySerializer
+class FeaturedMenuItem(ListAPIView):
+    queryset=MenuItem.objects.filter(is_featured=True)
+    serializer_class=MenuItemSerializer
 
